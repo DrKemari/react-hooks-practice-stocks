be44bb43936b6d43512d4cade06543a0a624d6e9
Matara Kemari <drkemari@gmail.com>
2025-08-20 08:32:44 +0300

    trials

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Stock() {
+function Stock({id,ticker,name,type,price,handleClick}) {
   return (
     <div>
       <div className="card">
-        <div className="card-body">
-          <h5 className="card-title">{"Compant Name"}</h5>
-          <p className="card-text">{"Stock Price"}</p>
+        <div className="card-body"id={id} onClick={handleClick}>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text">{price}</p>
         </div>
       </div>
     </div>
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+const[stocks, setStocks]=useState([])
+  useEffect(()=>{
+    fetch("http://localhost:3001/stocks")
+    .then((res)=>res.json())
+    .then((data)=>setStocks(data))
+    
+    },[])
+
+    function onclickdiv(id,stock){
+      alert(`Clicked`);
+      const updatedStock = { ...stock, portfolio: true }
+      fetch(`http://localhost:3001/stocks/${id}`,{
+        method:"PATCH",
+        headers:{ "Content-Type": "application/json" },
+        body:JSON.stringify(updatedStock)
+      })
+      .then((res) => res.json())
+      .then((newStock) => {
+        setStocks((prevStocks) =>
+          prevStocks.map((s) =>
+            s.id === newStock.id ? newStock : s
+          )
+        );
+      });
+    }
+    // function onclickdiv(stock){
+    //   alert(`Clicked`);
+    //   fetch("http://localhost:3001/stocks/",{
+    //     method:"POST",
+    //     headers:{ "Content-Type": "application/json" },
+    //     body:JSON.stringify(stock)
+    //   })
+    //   .then((res)=>res.json())
+    //   .then(newstock)
+
+    // }
+
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {stocks.map((stock)=>
+        <Stock
+        key={stock.id}
+        id={stock.id}
+        name={stock.name}
+        type={stock.type}
+        price={stock.price}
+        handleClick={() => onclickdiv(stock.id, stock)} // ✅ pass stock + id
+      />)}
     </div>
   );
 }
